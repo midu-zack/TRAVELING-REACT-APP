d84dfed211249e392fdc9b4c302b447290dd36e0
midu-zack <midlajmidu0002@gmail.com>
2024-05-30 10:07:24 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/pages/Home";
 import Services from "./components/pages/Services";
 import Products from "./components/pages/Products";
 import SignUp from "./components/pages/SignUp";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,10 +14,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-         <Route path="/services" element={<Services />} />  
+        <Route path="/services" element={<Services />} />
         <Route path="/products" element={<Products />} />
-        <Route path="/sign-up" element={<SignUp />} />  
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
+      {/* <Footer/> */}
     </Router>
   );
 }
